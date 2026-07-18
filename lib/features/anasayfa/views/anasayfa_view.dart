94beb8a19242dc7dd8f11dce2160a4d7c5e9c648
İrem Taşcı <iremtasci83@gmail.com>
2026-07-18 18:30:42 +0300

    arkaplan renk

--- lib/features/anasayfa/views/anasayfa_view.dart
+++ lib/features/anasayfa/views/anasayfa_view.dart
@@ -9,6 +9,7 @@ class AnasayfaView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Color(0xFFEBEDE3),
       appBar: CustomAppBar(
         title: 'Merhaba İrem',
         centerTitle: false,
