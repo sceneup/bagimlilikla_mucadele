e3ac3f3f8176a9002ae70ce4126d0fdb3a33670a
Tugbanurcan <tugba.n.can@gmail.com>
2026-08-10 19:44:06 +0300

    profil sayfası kaldırıldı

---
+++ lib/features/profil/ProfilView.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ProfilView extends StatelessWidget {
+  const ProfilView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
